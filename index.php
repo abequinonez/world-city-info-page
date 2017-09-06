50636a28d1a0a38295895566382a78c9080e75fb
abequinonez <abequinonez@users.noreply.github.com>
2017-09-06 04:47:31 -0700

    Changed URL in address bar example

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 			To retrieve city and other information from a given country, please select from the options below. Alternatively, you may enter the information into the address bar. For example, to retrieve U.S. cities with a population greater than 1,000,000 sorted by population, you can enter:
 		</p>
 		<div class="contained-code">
-			<pre>https://abequinonez.000webhostapp.com/embeddedsql.php?code=USA&pop=1000000&sort=city.Population&submit=submit#</pre>
+			<pre>https://abequinonez.000webhostapp.com/world/?code=USA&pop=1000000&sort=city.Population&submit=submit#</pre>
 		</div>
 		<form class="form-inline" action="#" method="GET">
 			<div class="form-group">
